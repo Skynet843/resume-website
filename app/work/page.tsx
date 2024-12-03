278213d69c2f14591129e2fffcdf9bf7d7dd8bab
Mahadeb Sen <mahadebsen237@gmail.com>
2024-12-03 22:27:13 +0530

    added necessary imports and project data.

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,4 +1,80 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+// projects data
+const projects = [
+  {
+    num: "01",
+    category: "Full-stack",
+    title: "project 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis repellat nobis excepturi quasi dicta nulla temporibus provident sit mollitia optio.",
+    stack: [
+      { name: "HTML 5" },
+      { name: "CSS 3" },
+      { name: "JavaScript" },
+      { name: "React.Js" },
+    ],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "Full-stack",
+    title: "project 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis repellat nobis excepturi quasi dicta nulla temporibus provident sit mollitia optio.",
+    stack: [
+      { name: "Next.Js" },
+      { name: "Tailwind css" },
+      { name: "React.Js" },
+      { name: "Node.Js" },
+    ],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "Full-stack",
+    title: "project 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis repellat nobis excepturi quasi dicta nulla temporibus provident sit mollitia optio.",
+    stack: [
+      { name: "HTML 5" },
+      { name: "CSS 3" },
+      { name: "JavaScript" },
+      { name: "React.Js" },
+      { name: "Next.Js" },
+      { name: "Tailwind css" },
+    ],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
+];
+
 const Work = () => {
+  const [project, setProject] = useState(projects[0]);
+
   return <div>Work page</div>;
 };
 
