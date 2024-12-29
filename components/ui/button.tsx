5feb25f496ba9649dae8978238f3222f4acd56f6
Mahadeb Sen <mahadebsen237@gmail.com>
2024-12-29 23:53:43 +0530

    Added style for button disable.

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-full text-base font-semibold ring-offset-white transition-colors",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-full text-base font-semibold ring-offset-white transition-colors disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
